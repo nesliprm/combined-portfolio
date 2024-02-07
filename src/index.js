158c99ccda711b4b811bc41707d13663f7f0b683
nesliprm <nesliprm@gmail.com>
2024-02-07 18:38:15 +0100

    added js, test images

---
+++ src/index.js
@@ -0,0 +1,24 @@
+let frontEnd = document.querySelector("#frontend-link");
+let arch = document.querySelector("#architect-link");
+
+frontEnd.addEventListener("mouseover", showImageFrontEnd);
+frontEnd.addEventListener("mouseleave", hideImageFrontEnd);
+
+arch.addEventListener("mouseover", showImageArch);
+arch.addEventListener("mouseleave", hideImageArch);
+
+function showImageFrontEnd() {
+  frontEnd.classList.add("frontend-link-hover");
+}
+
+function hideImageFrontEnd() {
+  frontEnd.classList.remove("frontend-link-hover");
+}
+
+function showImageArch() {
+  arch.classList.add("architecture-link-hover");
+}
+
+function hideImageArch() {
+  arch.classList.remove("architecture-link-hover");
+}
